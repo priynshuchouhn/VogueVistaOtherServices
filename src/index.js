fd7fb1542a2ca4897ad1a1014484c49763677d31
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-02-29 23:07:50 +0530

    APP: Endpoint Added to Start the Service

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log("Service running at "+ PORT);
+})
+app.get('/', (req,res,next)=>{
+    res.send('Server Started');
+})
 const emailWorker = require('./queues/email-queue');
 console.log('Service is running....');
