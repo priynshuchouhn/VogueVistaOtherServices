e43d8bf84b509f3f52afbee00e0642398c86839c
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-03-10 22:49:07 +0530

    App: minor fixes for push notification

--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ const webpush = require('web-push');
 const app = express();
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, ()=>{
-    console.log("Service running at "+ PORT);
-    db();
-
-})
+db().then(res => {
+    app.listen(PORT, ()=>{
+        console.log("Service running at "+ PORT);
+       
+    })
+});
 app.get('/', (req,res,next)=>{
     res.send('Server Started');
+})
+app.get('/notify', (req,res,next)=>{
+    sendMessageToAll();
+    res.send('Message send to all users');
 })
 const emailWorker = require('./queues/email-queue');
 console.log('Service is running....');
